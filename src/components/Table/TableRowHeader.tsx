3d0118c0223e502331494d8400e1fdeb3acb0669
lhelheo <leonardo.villalva@academico.ufs.br>
2024-04-12 10:33:41 -0300

    att

--- src/components/Table/TableRowHeader.tsx
+++ src/components/Table/TableRowHeader.tsx
@@ -1,20 +1,18 @@
-
+import data from '../../data.json';
 const thStyle = "px-6 py-3 text-left text-xs font-medium text-gray-500";
 
 const TableRowHeader = () => {
+  const headers = data.length > 0 ? Object.keys(data[0]) : [];
+
   return (
     <tr>
-        <th scope="col" className={thStyle}>
-            Nome
+      {headers.map(header => (
+        <th key={header} scope="col" className={thStyle}>
+          {header.charAt(0).toUpperCase() + header.slice(1)}
         </th>
-        <th scope="col" className={thStyle}>
-            Idade
-        </th>
-        <th scope="col" className={thStyle}>
-            Ação
-        </th>
-    </tr>         
-  )
-}
+      ))}
+    </tr>
+  );
+};
 
-export default TableRowHeader
+export default TableRowHeader;
