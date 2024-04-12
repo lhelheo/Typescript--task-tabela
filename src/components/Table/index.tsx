d34b6eb30e3255f9f26148fd952706fb04627b9a
Leonardo Oliveira Villalva <leonardo.villalva@academico.ufs.br>
2024-04-12 06:18:26 -0300

    atualização testando ordenação e corrigindo conflito de ordenação e testando barra de pesquisa

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -3,15 +3,60 @@ import TableRow from './TableRow';
 import data from '../../data.json';
 import TableHeader from './TableHeader';
 import Searchbar from '../Searchbar';
+import { ArrowDown, ArrowUp, Trash } from 'lucide-react';
 
 const Table = () => {
   const [filterText, setFilterText] = useState('');
+  const [orderByAge, setOrderByAge] = useState('default');
+  const [orderByName, setOrderByName] = useState('default');
 
   const handleFilterChange = (event) => {
     setFilterText(event.target.value.toLowerCase());
   };
 
-  const filteredData = data.filter(person => {
+  const handleOrderByAge = () => {
+    switch (orderByAge) {
+      case 'asc':
+        setOrderByAge('desc');
+        break;
+      case 'desc':
+        setOrderByAge('');
+        break;
+      default:
+        setOrderByName('default');
+        setOrderByAge('asc');
+    }
+  };
+
+  const handleOrderByName = () => {
+    switch (orderByName) {
+      case 'asc':
+        setOrderByName('desc');
+        break;
+      case 'desc':
+        setOrderByName('');
+        break;
+      default:
+        setOrderByAge('default');
+        setOrderByName('asc');
+    }
+  };
+
+  let sortedData = [...data];
+
+  if (orderByAge === 'asc') {
+    sortedData = sortedData.sort((a, b) => a.age - b.age);
+  } else if (orderByAge === 'desc') {
+    sortedData = sortedData.sort((a, b) => b.age - a.age);
+  }
+
+  if (orderByName === 'asc') {
+    sortedData = sortedData.sort((a, b) => a.name.localeCompare(b.name));
+  } else if (orderByName === 'desc') {
+    sortedData = sortedData.sort((a, b) => b.name.localeCompare(a.name));
+  }
+
+  const filteredData = sortedData.filter(person => {
     return person.name.toLowerCase().includes(filterText) ||
            person.age.toString().includes(filterText);
   });
@@ -32,9 +77,11 @@ const Table = () => {
             className="p-2"
           />
         </label>
-        <button className="m-2" onClick={() => setFilterText('')}>
-          clear
-        </button>
+        
+        
+        <button className='flex' onClick={handleOrderByAge}>Ordenar por Idade {orderByAge === 'asc' ? <ArrowUp /> : orderByAge === 'desc' ? <ArrowDown /> : ' ' }</button>
+        <button className='flex' onClick={handleOrderByName}>Ordenar por Nome {orderByName === 'asc' ? <ArrowUp /> : orderByName === 'desc' ? <ArrowDown /> : ' '}</button>
+        <Trash className="cursor-pointer text-gray-500" onClick={() => setFilterText('')}/>
       </div>
       <div className="relative overflow-x-auto w-min mx-auto">
         <table className="w-full divide-y divide-gray-200 shadow-xl rounded-md">  
